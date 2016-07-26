1c62d2adfbff38e0fbd1064cad961d523f7d4f6c
derushio <narumi18wa@gmail.com>
2016-07-26 19:16:18 +0900

    deployBlocks 関数化

--- main.lua
+++ main.lua
@@ -73,28 +73,55 @@ ball:setLinearVelocity(0, 500)
 
 -- ############################## ブロックを配置してみよう ##############################
 
+local maxNumBlocks = 0
 local numBlocks = 0
+
 local blocks = {}
 
-for y = 0, 1, 1 do
-    for x = 0, 4, 1 do
-        -- 何番目の要素か
-        local index = x + (y * 5)
-        blocks[index] = display.newImageRect(displayGroup, "block.jpg", width * 1/8, 100)
-        blocks[index].x = (x + 1) * (width * 1/6)
-        blocks[index].y = 400 + (200 * y)
-        blocks[index].tag = "block"
-        blocks[index].index = index
-        physics.addBody(blocks[index], "static", {density = 0.0, friction = 0.0, bounce = 1.0})
-        numBlocks = numBlocks + 1
-    end
-end
-
--- ブロックを削除する処理 後で使う
 local function deleteBlock(index)
+    if (blocks[index] == nil) then
+        return
+    end
+
     blocks[index]:removeSelf()
+    blocks[index] = nil
     numBlocks = numBlocks - 1
 end
+
+local function deleteAllBlocks()
+    for i = 0, maxNumBlocks, 1 do
+        deleteBlock(i)
+    end
+
+    maxNumBlocks = 0
+    numBlocks = 0
+    blocks = {}
+end
+
+local function deployBlocks()
+    -- ブロックを配置する前に全てのブロックを削除
+    deleteAllBlocks()
+
+    -- ブロックを配置
+    for y = 0, 1, 1 do
+        for x = 0, 4, 1 do
+            -- 何番目の要素か
+            local index = x + (y * 5)
+            blocks[index] = display.newImageRect(displayGroup, "block.jpg", width * 1/8, 100)
+            blocks[index].x = (x + 1) * (width * 1/6)
+            blocks[index].y = 400 + (200 * y)
+            blocks[index].tag = "block"
+            blocks[index].index = index
+            physics.addBody(blocks[index], "static", {density = 0.0, friction = 0.0, bounce = 1.0})
+
+            numBlocks = numBlocks + 1
+        end
+    end
+
+    maxNumBlocks = numBlocks
+end
+
+deployBlocks()
 
 -- ############################## ブロックを配置してみよう ##############################
 
